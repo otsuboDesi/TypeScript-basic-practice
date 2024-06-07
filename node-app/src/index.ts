40b27c49fe11c2ac867fbcbb28195d29b572cf71
otsuboDesi <otsubodesignare@gmail.com>
2024-06-07 17:26:13 +0900

    promptinput関数の追加

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -9,3 +9,13 @@ process.stdout.write(sayHello(`Michael Jackson`));
 const printLine = (text: string, breakLine: boolean = true) => {
   process.stdout.write(text + (breakLine ? "\n" : ""));
 };
+
+// ユーザーに質問を投げかけ、入力をしてもらう関数
+const promptInput = async (text: string) => {
+  printLine(`\n${text}\n>`, false);
+
+  const input: string = await new Promise((resolve) =>
+    process.stdin.once("data", (data) => resolve(data.toString()))
+  );
+  return input.trim();
+};
