7aac4b1afce7761e99422d6766b8d0187bb89e0d
otsuboDesi <otsubodesignare@gmail.com>
2024-06-07 17:38:50 +0900

    HitANdBlowクラスの追加

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -19,3 +19,15 @@ const promptInput = async (text: string) => {
   console.log(age);
   process.exit();
 })();
+
+class HitAndBlow {
+  answerSource: string[];
+  answer: string[];
+  tryCount: number;
+
+  constructor() {
+    this.answerSource = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    this.answer = [];
+    this.tryCount = 0;
+  }
+}
