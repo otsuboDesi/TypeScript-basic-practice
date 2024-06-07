2f43f2d7875d65de0f292c32afbe81b7795863bb
otsuboDesi <otsubodesignare@gmail.com>
2024-06-07 17:35:46 +0900

    add 動作確認コード

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,21 +1,21 @@
-const sayHello = (name: string) => {
-  return `Hello, ${name}`;
-};
-
-console.log(sayHello(`Michael Jackson`));
-
-process.stdout.write(sayHello(`Michael Jackson`));
-
 const printLine = (text: string, breakLine: boolean = true) => {
   process.stdout.write(text + (breakLine ? "\n" : ""));
 };
 
 // ユーザーに質問を投げかけ、入力をしてもらう関数
 const promptInput = async (text: string) => {
-  printLine(`\n${text}\n>`, false);
+  printLine(`\n${text}\n> `, false);
 
   const input: string = await new Promise((resolve) =>
     process.stdin.once("data", (data) => resolve(data.toString()))
   );
   return input.trim();
 };
+
+(async () => {
+  const name = await promptInput("名前を入力してください");
+  console.log(name);
+  const age = await promptInput("年齢を入力してください");
+  console.log(age);
+  process.exit();
+})();
