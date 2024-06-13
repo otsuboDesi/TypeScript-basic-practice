573612b3a9e3d07fed4b92aa8fc2735a0097378c
otsuboDesi <otsubodesignare@gmail.com>
2024-06-13 15:43:49 +0900

    Add: validate()の追加

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -49,6 +49,13 @@ class HitAndBlow {
     const inputArr = (
       await promptInput("「,」区切りで3つの数字を入力してください")
     ).split(",");
+
+    if (!this.validate(inputArr)) {
+      printLine("無効な入力です。");
+      await this.play();
+      return;
+    }
+
     const result = this.check(inputArr);
 
     if (result.hit !== this.answer.length) {
@@ -86,6 +93,18 @@ class HitAndBlow {
       hit: hitCount,
       blow: blowCount,
     };
+  }
+
+  // validationの追加
+  private validate(inputArr: string[]) {
+    const isLengthValid = inputArr.length === this.answer.length;
+    const isAllAnswerSourceOption = inputArr.every((val) =>
+      this.answerSource.includes(val)
+    );
+    const isAllDifferentValues = inputArr.every(
+      (val, i) => inputArr.indexOf(val) === i
+    );
+    return isLengthValid && isAllAnswerSourceOption && isAllDifferentValues;
   }
 }
 
