a5d60b9a86c72357cc276eccc3deb7bb8094acca
otsuboDesi <otsubodesignare@gmail.com>
2024-06-17 23:32:54 +0900

    as const, keyof, typeofを使って列挙型を再現する

--- browser-app/ts/Task.ts
+++ browser-app/ts/Task.ts
@@ -1,11 +1,25 @@
 import { v4 as uuid } from "uuid";
 
+export const statusMap = {
+  todo: "TODO",
+  doing: "DOING",
+  done: "DONE",
+  //   as constで型アサーションを行うので、"TODO"型、"DOING"型、 "DONE"型になる
+} as const;
+
+// keyof: ユニオン型にする
+// typeof: 変数の型を取り出すことができる
+export type Status = (typeof statusMap)[keyof typeof statusMap];
+// output: "TODO" | "DOING" | "DONE"
+
 export class Task {
   readonly id;
   title;
+  status;
 
   constructor(properties: { title: string }) {
     this.id = uuid();
     this.title = properties.title;
+    this.status = statusMap.todo;
   }
 }
