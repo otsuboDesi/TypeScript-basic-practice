5bfff7df42d1b0fb275eed2ca616da7f987279e1
otsuboDesi <otsubodesignare@gmail.com>
2024-06-17 13:46:48 +0900

    Application classを準備する

--- browser-app/ts/index.ts
+++ browser-app/ts/index.ts
@@ -1,4 +1,14 @@
-import { sum } from "./sum";
+class Application {
+  start() {
+    console.log("hello world");
+  }
+}
 
-console.log(sum(1, 2));
-console.log("Hello World");
+// windowsのloadイベントのハンドラでApplicationクラスのインスタンスを作成
+// loadはローディング工程がすべて終わった後に呼ばれるイベント
+// loadが呼ばれた時点で、HTMLは全てDOMとして取得でき、画像などのassetsのロードも完了している
+window.addEventListener("load", () => {
+  const app = new Application();
+  // start()を実行する
+  app.start();
+});
