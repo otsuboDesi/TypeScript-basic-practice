1853a7f1a51e1b70f2ce54eb8041f8f5eab73534
otsuboDesi <otsubodesignare@gmail.com>
2024-06-20 14:23:15 +0900

    サンプルコードをコメントアウト

--- browser-app/ts/@types/uuid/index.d.ts
+++ browser-app/ts/@types/uuid/index.d.ts
@@ -2,9 +2,9 @@
 // 使いたいライブラリの型定義がDefinitely Typedに存在しない時は、自分でライブラリの型定義ファイルを作成しなければならない
 
 // Definitely Typed
-declare module "uuid" {
-  namespace uuid {
-    function v4(): string;
-  }
-  export = uuid;
-}
+// declare module "uuid" {
+//   namespace uuid {
+//     function v4(): string;
+//   }
+//   export = uuid;
+// }
