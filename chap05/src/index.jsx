d6838a39e4c2049f75dd6f2d94fb748b05aefb5e
otsuboDesi <otsubodesignare@gmail.com>
2024-06-21 12:00:32 +0900

    Reactアプリケーションのひな型作成

---
+++ chap05/src/index.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import { App } from "./App";
+
+// ReactDOM.renderによって、HTMLファイルにレンダリング対象となる要素をdocument.getElementById('app')で指定
+// 指定されたファイルはindex.htmlのことを指す
+// そしてreactコンポーネントをレンダリングしている
+ReactDOM.render(<App />, document.getElementById("app"));
